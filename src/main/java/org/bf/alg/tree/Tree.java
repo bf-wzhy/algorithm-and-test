0b3942cba4ece1970a47ba6b7a865a6c19c8185d
bf-wzhy <wzhy0576@163.com>
2021-09-22 10:23:44 +0800

    define Tree interface

--- src/main/java/org/bf/alg/tree/Tree.java
+++ src/main/java/org/bf/alg/tree/Tree.java
@@ -3,6 +3,15 @@ package org.bf.alg.tree;
 import org.jetbrains.annotations.NotNull;
 
 public interface Tree<K> {
+    /**
+     * Insert a key into tree.
+     * @param key needs inserting
+     */
     void insert(@NotNull K key);
+
+    /**
+     * Delete a key from tree.
+     * @param key needs deleting
+     */
     void delete(@NotNull K key);
 }
